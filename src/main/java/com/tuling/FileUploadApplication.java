82d0ec699c93e2aa37cf77622924dcbb00a0154f
SuJ1213 <878242569@qq.com>
2024-10-27 20:45:06 +0800

    大文件上传下载v1.0

---
+++ src/main/java/com/tuling/FileUploadApplication.java
@@ -0,0 +1,15 @@
+package com.tuling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 苏佳
+ * @Date ${YEAR} ${MONTH} ${DAY} ${HOUR} ${MINUTE}
+ **/
+@SpringBootApplication
+public class FileUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadApplication.class,args);
+    }
+}
